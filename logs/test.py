13d4c0b88d891eb6cde206d6fd9869517c81ba29
Skobarev A.A <skobarev.aa@gmail.com>
2023-09-13 00:27:43 +0300

    new version with AVG volumes

--- logs/test.py
+++
@@ -1,8 +0,0 @@
-def _get_futures_socket(self, path: str, futures_type: FuturesType, prefix: str = 'stream?streams='):
-    socket_type: BinanceSocketType = BinanceSocketType.USD_M_FUTURES
-    if futures_type == FuturesType.USD_M:
-        stream_url = self.FSTREAM_URL
-        if self.testnet:
-            stream_url = self.FSTREAM_TESTNET_URL
-    else:
-        stream_url = self.DSTREAM_URL
